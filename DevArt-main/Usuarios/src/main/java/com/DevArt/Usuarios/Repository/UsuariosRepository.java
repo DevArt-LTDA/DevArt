57cdfdd834f4f9dd15fbbaedb4abc1e75f8fbde5
Elian05 <el.barram@duocuc.cl>
2025-05-17 23:23:36 -0400

    Cambio Nombre Carpeta repository a minuscula

--- DevArt-main/Usuarios/src/main/java/com/DevArt/Usuarios/Repository/UsuariosRepository.java
+++ DevArt-main/Usuarios/src/main/java/com/DevArt/Usuarios/Repository/UsuariosRepository.java
@@ -1,9 +1,11 @@
 package com.DevArt.Usuarios.repository;
 import com.DevArt.Usuarios.model.Usuarios;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+
 
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, String> {
