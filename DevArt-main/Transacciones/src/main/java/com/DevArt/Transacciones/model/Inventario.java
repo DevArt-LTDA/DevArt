562d56d575b4fe0015bade47bb2b673265d31bc3
Alorca <aa.lorca@duocuc.cl>
2025-05-24 10:19:48 -0400

    Clase Inventario terminada rut, horas

--- DevArt-main/Transacciones/src/main/java/com/DevArt/Transacciones/model/Inventario.java
+++ DevArt-main/Transacciones/src/main/java/com/DevArt/Transacciones/model/Inventario.java
@@ -1,5 +1,22 @@
 package com.DevArt.Transacciones.model;
 
-public class Inventario {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "iventarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventario {
+    @Id
+    @Column(name = "rut_programador", nullable = false, length = 255)
+    private String rutProgramador;
+    @Column(name = "horas_disponibles", nullable = false, length = 100)
+    private int  horaDisponible;
 }
