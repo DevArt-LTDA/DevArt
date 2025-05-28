a1c842294bcbea0df3e44609de59cef575168a9b
CesarE <ce.estay@duocuc.cl>
2025-05-28 03:01:43 -0400

    Arreglos UsuarioController

--- DevArt-main/Usuarios/src/main/java/com/DevArt/Usuarios/controller/UsuariosController.java
+++ DevArt-main/Usuarios/src/main/java/com/DevArt/Usuarios/controller/UsuariosController.java
@@ -43,7 +43,7 @@ public class UsuariosController {
     }
 
     @GetMapping("/nombre/{primerNombre}")
-    public ResponseEntity<List<Usuarios>> getUsuarioByPrimerNombre(@PathVariable("primeroNombre") String primerNombre) {
+    public ResponseEntity<List<Usuarios>> getUsuarioByPrimerNombre(@PathVariable("primerNombre") String primerNombre) {
         List<Usuarios> usuarios = usuarioService.getUsuarioByPrimerNombre(primerNombre);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -52,23 +52,23 @@ public class UsuariosController {
     }
 
     @GetMapping("/segundoNombre/{segundoNombre}")
-    public ResponseEntity<List<Usuarios>> getUsuarioBySegundoNombre(@PathVariable("rut") String segundoNombre) {
+    public ResponseEntity<List<Usuarios>> getUsuarioBySegundoNombre(@PathVariable("segundoNombre") String segundoNombre) {
         List<Usuarios> usuarios = usuarioService.getUsuarioBySegundoNombre(segundoNombre);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(usuarios);
     }
-    @GetMapping("/apellido/{primerApellido}")
-    public ResponseEntity<List<Usuarios>> getUsuarioByPrimerApellido(@PathVariable("rut") String primerApellido) {
+    @GetMapping("/primerApellido/{primerApellido}")
+    public ResponseEntity<List<Usuarios>> getUsuarioByPrimerApellido(@PathVariable("primerApellido") String primerApellido) {
         List<Usuarios> usuarios = usuarioService.getUsuarioByPrimerApellido(primerApellido);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(usuarios);
     }
-    @GetMapping("/segundo-apellido/{segundoApellido}")
-    public ResponseEntity<List<Usuarios>> getUsuarioBySegundoApellido(@PathVariable("rut") String segundoApellido) {
+    @GetMapping("/segundoApellido/{segundoApellido}")
+    public ResponseEntity<List<Usuarios>> getUsuarioBySegundoApellido(@PathVariable("segundoApellido") String segundoApellido) {
         List<Usuarios> usuarios = usuarioService.getUsuarioBySegundoApellido(segundoApellido);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -76,7 +76,7 @@ public class UsuariosController {
         return ResponseEntity.ok(usuarios);
     }
     @GetMapping("/correo/{correo}")
-    public ResponseEntity<List<Usuarios>> getUsuarioByCorreo(@PathVariable("rut") String correo) {
+    public ResponseEntity<List<Usuarios>> getUsuarioByCorreo(@PathVariable("correo") String correo) {
         List<Usuarios> usuarios = usuarioService.getUsuarioByCorreo(correo);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -84,7 +84,7 @@ public class UsuariosController {
         return ResponseEntity.ok(usuarios);
     }
     @GetMapping("/fechaNacimiento/{fechaNacimiento}")
-    public ResponseEntity<List<Usuarios>> getUsuarioByFechaNacimiento(@PathVariable("rut") String fechaNacimiento) {
+    public ResponseEntity<List<Usuarios>> getUsuarioByFechaNacimiento(@PathVariable("fechaNacimiento") String fechaNacimiento) {
         List<Usuarios> usuarios = usuarioService.getUsuarioByFechaNacimiento(fechaNacimiento);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -92,7 +92,7 @@ public class UsuariosController {
         return ResponseEntity.ok(usuarios);
     }
     @GetMapping("/telefono/{telefono}")
-    public ResponseEntity<List<Usuarios>> getUsuarioByTelefono(@PathVariable("rut") String telefono) {
+    public ResponseEntity<List<Usuarios>> getUsuarioByTelefono(@PathVariable("telefono") String telefono) {
         List<Usuarios> usuarios = usuarioService.getUsuarioByTelefono(telefono);
         if (usuarios.isEmpty()) {
             return ResponseEntity.notFound().build();
