d29bcbb2eb99743721008bd0369b1d6f5b69cc6f
Elian05 <el.barram@duocuc.cl>
2025-06-07 11:02:42 -0400

    Implementacion Test Usuario

--- DevArt-main/Usuarios/src/test/java/com/DevArt/Usuarios/UsuariosApplicationTests.java
+++ DevArt-main/Usuarios/src/test/java/com/DevArt/Usuarios/UsuariosApplicationTests.java
@@ -1,13 +1,41 @@
 package com.DevArt.Usuarios;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import com.DevArt.Usuarios.controller.UsuariosController;
 
-@SpringBootTest
-class UsuariosApplicationTests {
+// Java
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.profiles.active=dev" })
+public class UsuariosApplicationTests {
+
+	@Autowired
+	private UsuariosController usuarioController;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		System.out.println("Testing the context loading...");
 	}
 
+	@Test
+	public void contextLoads2() throws Exception {
+		System.out.println("Testing the context loading. and the controller...");
+		assertThat(usuarioController).isNotNull();
+	}
+
+	@Test
+	public void getLibrosContainsBrackets() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port +
+				"/api/v1/usuarios",
+				String.class)).contains("[");
+	}
 }
