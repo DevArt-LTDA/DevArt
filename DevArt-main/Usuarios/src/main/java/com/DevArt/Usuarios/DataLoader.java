9f272f2242d953f80bd22cf7d35f98ac20384f0e
Elian05 <el.barram@duocuc.cl>
2025-06-16 19:23:43 -0400

    PushSecretsBasesdeDatos

--- DevArt-main/Usuarios/src/main/java/com/DevArt/Usuarios/DataLoader.java
+++ DevArt-main/Usuarios/src/main/java/com/DevArt/Usuarios/DataLoader.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import java.time.LocalDate;
 
 
 // import java.util.Date;
